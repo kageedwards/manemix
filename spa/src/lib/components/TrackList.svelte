0f7c049ac9cb01b8e3e95f7a5fe021adb45c297c
Kage Edwards <kagemykel.edwards@gmail.com>
2026-04-01 00:47:35 -0700

    placeholder when tracklist is empty

--- spa/src/lib/components/TrackList.svelte
+++ spa/src/lib/components/TrackList.svelte
@@ -13,4 +13,7 @@
   {#each tracks as track (track.tid)}
     <TrackCard {track} siblingTracks={tracks} {playbackContext} />
   {/each}
+  {#if tracks.length === 0}
+    <p class="text-sm opacity-50 py-4">No tracks to show.</p>
+  {/if}
 </div>
